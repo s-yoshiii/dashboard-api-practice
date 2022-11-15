9ff2bf7053018f25f4b951c957f3d7153e7f29c5
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-11-16 00:40:47 +0900

    graphSlice

---
+++ src/features/graph/graphSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "../../app/store";
+import dataDaily from "./data.json";
+const apiUrl = "https://opendata.corona.go.jp/api/Covid19JapanAll";
+type DATADAILY = typeof dataDaily;
+type covid19JapanState = {
+  daily: DATADAILY;
+  pref: string;
+};
+const initialState: covid19JapanState = {
+  daily: dataDaily,
+  pref: "東京都",
+};
+
+export const fetchAsyncGetDaily = createAsyncThunk(
+  "covid/getDaily",
+  async (pref: string) => {
+    const { data } = await axios.get<DATADAILY>(`${apiUrl}?dataName=${pref}`);
+    return { data: data, pref: pref };
+  }
+);
