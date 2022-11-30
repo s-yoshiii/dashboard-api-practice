5883f3b96e1626e3d79e8be2cfc905bdce394f83
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-01 00:51:17 +0900

    Cartコンポーネントの調整

--- src/features/graph/Chart/Chart.tsx
+++ src/features/graph/Chart/Chart.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { Chart, registerables } from "chart.js";
+import { Line } from "react-chartjs-2";
 import { useSelector } from "react-redux";
 import { selectDaily } from "../graphSlice";
 
-const Chart = () => {
+Chart.register(...registerables);
+const Charta = () => {
   const daily = useSelector(selectDaily);
-  console.log(daily);
   const dates = daily.map(({ Date }) => Date);
-  return <div>Chart</div>;
+  console.log(daily);
+  const lineChart = daily[0] && (
+    <Line
+      data={{
+        labels: dates.map((data) => new Date(data).toString()),
+        datasets: [
+          {
+            label: "Infected",
+            data: daily.map((data) => data.Confirmed),
+            fill: false,
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+          },
+        ],
+      }}
+    />
+  );
+  return <div>{lineChart}</div>;
 };
 
-export default Chart;
+export default Charta;
