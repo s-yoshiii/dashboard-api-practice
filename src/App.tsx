93018767f9dcefe6d63f5b46f46470c54b05ad15
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-12-05 09:49:05 +0900

    SwitchCountryAPIデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Container } from "@mui/system";
 import Cards from "./features/graph/Cards/Cards";
 import LineChart from "./features/graph/LineChart/LineChart";
 import PieChart from "./features/graph/PieChart/PieChart";
+import SwitchCountry from "./features/graph/SwitchCountry/SwitchCountry";
 
 function App() {
   return (
     <Container maxWidth="lg">
       <Cards />
+      <SwitchCountry />
       <LineChart />
       <PieChart />
     </Container>
--- src/features/graph/SwitchCountry/SwitchCountry.tsx
+++ src/features/graph/SwitchCountry/SwitchCountry.tsx
@@ -4,9 +4,33 @@ import { styled } from "@mui/material/styles";
 import { useAppDispatch } from "../../../app/hooks";
 import { fetchAsyncGetDaily } from "../graphSlice";
 const SwitchCountry: FC = () => {
+  const dispatch = useAppDispatch();
+  const contries = [
+    "japan",
+    "united-states",
+    "germany",
+    "new-zealand",
+    "australia",
+    "china",
+    "vietnam",
+    "thailand",
+  ];
   return (
     <div>
-      <FormControl>aaaa</FormControl>;
+      <FormControl>
+        <NativeSelect
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+            dispatch(fetchAsyncGetDaily(e.target.value))
+          }
+        >
+          {contries.map((contry, i) => (
+            <option key={i} value={contry}>
+              {contry}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+      ;
     </div>
   );
 };
